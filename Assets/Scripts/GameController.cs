cf105266f07676841d69419bb31ab1477f0b4e41
tsunamit <DrTsunami@users.noreply.github.com>
2018-03-31 22:37:41 -0700

    selection and summoning and raycasting all functional

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,9 +6,12 @@ public class GameController : MonoBehaviour {
 
     // Variables: specific to testing
     string mainObjectString = "TestCube";
+    GameObject leapController;
+    Camera camera;
 
-    // Multipliers for gestures 
+    // vars for gestures 
     public float rotationMultiplier = -0.01f;
+    bool objectIsSelected = false;                                 
 
     // Leap Objects
     LeapListener listener;
@@ -21,23 +24,97 @@ public class GameController : MonoBehaviour {
         Debug.Log("GameController: RUNNING");
 
         // Find game objects and assign
+        camera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        leapController = GameObject.Find("LeapHandController");
         listener = GameObject.Find("LeapListener").GetComponent<LeapListener>();
         mainObject = GameObject.Find(mainObjectString);
 
         // Verify that objects have been found
         if (listener != null) Debug.Log("Listener: LISTENING");
         if (mainObject != null) Debug.Log("mainObject: IDENTIFIED");
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        // Rotate object
+        // ---------------------------------------------------------------------------------------------------------------------
+        // Rotate object: if LH grabs
         // TODO: only if in full body view
-        if (listener.rGrabbing)
+        if (listener.lGrabbing)
         {
-            mainObject.transform.rotation *=  Quaternion.Euler(0, rotationMultiplier * listener.R_GetPalmVelocity().x, 0);
+            mainObject.transform.rotation *=  Quaternion.Euler(0, rotationMultiplier * listener.L_GetPalmVelocity().x, 0);
         }
-	}
+
+
+
+        // ---------------------------------------------------------------------------------------------------------------------
+        // TODO: Summon motion: Object selected
+        // TODO: need to make sure that an object is selected
+
+
+        
+    }
+
+
+
+    GameObject selected;
+
+    private void FixedUpdate()
+    {
+
+        // ---------------------------------------------------------------------------------------------------------------------
+        // RayCast from palm position
+        if (listener.rInView)
+        {
+            float shaving = 0.5f;
+            Vector3 rayMod = new Vector3(0, 0.2f, -0.5f);
+            Vector3 rayDir = new Vector3(listener.GetArmDir().x * shaving, listener.GetArmDir().y, listener.GetArmDir().z * -1.0f);
+
+            Ray r = new Ray(leapController.transform.position + rayMod, rayDir * 200.0f);
+            Debug.DrawRay(leapController.transform.position + rayMod, rayDir * 200.0f);
+
+            RaycastHit hit;
+            if (Physics.Raycast(r, out hit))
+            {
+                if (hit.collider.tag == "orb")
+                {
+                    if (listener.selectionGestureDetected)
+                    {
+                        selected = hit.collider.gameObject;
+                    }
+                }
+            }
+            
+            
+        }
+
+        if (selected != null)
+        { 
+      
+            if (listener.rInView)
+            {
+                if (listener.summonGestureDetected)
+                {
+                    selected.transform.position = camera.transform.position + new Vector3(0,0,2.0f);
+                }
+            }
+        }
+
+        
+       
+
+
+
+
+    }
+
+    // leap vector to unity vector
+    Vector3 lv_t_uv(Leap.Vector lv)
+    {
+        return new Vector3(lv.x, lv.y, lv.z);
+    }
+
+   
 
 }
--- Assets/Scripts/LeapListener.cs
+++ Assets/Scripts/LeapListener.cs
@@ -9,15 +9,26 @@ public class LeapListener : MonoBehaviour {
     Controller controller;
 
     // Constant Variables that may need changing
-    float normalDownThresh = -0.9f;
+    float normalUpThresh = 0.9f;
     float grabThresh = 0.7f;
+    public float summonThresh = 0.9f;
+    public float summonPalmNormThresh = 0.7f;
+    float leapToWorldMultiplier = 0.5f;
 
     public bool rInView;        // rh in view
     public bool lInView;        // lh in view
-    public bool rNormalDown;    // rh oriented palm down
-    public bool lNormalDown;    // lh oriented palm down
+    public bool rSummonReady;      // rh oriented palm down
+    public bool lSummonReady;      // lh oriented palm down
     public bool rGrabbing;      // rh grabbing gesture
     public bool lGrabbing;      // lh grabbing gesture
+
+    // advanced
+    public bool summonGestureDetected;
+    public bool selectionGestureDetected;
+
+    // buttonbool for one time actions
+    bool summoning = false;
+    bool selecting = false;
 
     
 
@@ -45,7 +56,14 @@ public class LeapListener : MonoBehaviour {
         CheckHandVisibility(hands);
         CheckPalmOrientation(hands);
         CheckGrab(hands);
-	}
+        ListenForSummon(rh);            // RH summon
+        ListenForSelection(rh);         // RH selection
+        // UpdateRingFingerPos(rh);        // Update the ring finger position .... NOT USING
+
+        // LeapToWorld(rh.PalmPosition);
+
+
+    }
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////
     // Rudimentary Gesture Detection: Checking States of Hands
@@ -81,19 +99,20 @@ public class LeapListener : MonoBehaviour {
      */
     void CheckPalmOrientation(List<Hand> hands)
     {
-        rNormalDown = false;
-        lNormalDown = false;
+        rSummonReady = false;
+        lSummonReady = false;
+        
 
         foreach (Hand h in hands)
         {
-            if (h.PalmNormal.y < normalDownThresh && h.IsRight)
+            if (h.PalmNormal.y > summonPalmNormThresh && h.IsRight)
             {
-                rNormalDown = true;
+                rSummonReady = true;
             }
 
-            else if (h.PalmNormal.y < normalDownThresh && h.IsLeft)
+            else if (h.PalmNormal.y > summonPalmNormThresh && h.IsLeft)
             {
-                rNormalDown = true;
+                rSummonReady = true;
             }
         }
     }
@@ -121,28 +140,123 @@ public class LeapListener : MonoBehaviour {
     }
 
     /**--------------------------------------------------------------------------------------
-     * Get the vector
+     * Getter for arm cast
+     */
+    public Leap.Vector GetArmDir()
+    {
+        return rh.Arm.Direction * 0.1f ;
+    }
+
+    public Leap.Vector GetElbowPos()
+    {
+        return rh.Arm.ElbowPosition;
+    }
+
+    public Leap.Vector GetFingDir()
+    {
+        return rh.Fingers[4].Direction * 0.1f;
+    }
+
+    /**--------------------------------------------------------------------------------------
+     * Getters for palm position, normal and velocity for both hands
      */
     public Leap.Vector R_GetPalmPosition()
     {
         //Debug.Log(rh.PalmPosition.x);
-        return rh.PalmPosition;
+        return LeapToWorld(rh.PalmPosition);
     }
 
     public Leap.Vector R_GetPalmVelocity()
     {
-        Debug.Log(rh.PalmVelocity.x);
         return rh.PalmVelocity;
     }
+
+    public Leap.Vector R_GetPalmNormal()
+    {
+        return rh.PalmNormal;
+    }
+
+    public Leap.Vector L_GetPalmNormal()
+    {
+        return lh.PalmNormal;
+    }
+
+    public Leap.Vector L_GetPalmVelocity()
+    {
+        return lh.PalmVelocity;
+    }
+    
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////
     // Advanced Gesture Detection
     /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    
+    /**--------------------------------------------------------------------------------------
+     * Getters for palm position and velocity for both hands
+     */
+    void ListenForSummon(Hand rh)
+    {
+        summonGestureDetected = false;
 
-    
-    
+        if (rh != null)
+        {
+            // if right palm normal facing upwards
+            if (rh.PalmNormal.y > summonPalmNormThresh && rh.GrabStrength > summonThresh && !summoning)
+            {
+                summonGestureDetected = true;
+                summoning = true;
+                Debug.Log("Summoning...");
+            }
+
+            else if (!(rh.PalmNormal.y >= summonPalmNormThresh && rh.GrabStrength > summonThresh) && summoning)
+            {
+                summoning = false;
+                Debug.Log("No longer summoning");
+            }
+            
+            //Debug.Log(rh.PalmNormal);
+        }
+    }
+
+    /**--------------------------------------------------------------------------------------
+     * Listens for a selection motion and sets selectiong to true
+     */
+    void ListenForSelection (Hand rh)
+    {
+
+        selectionGestureDetected = false;
+
+        if (rh != null)
+        {
+            // if RH grabbing and the normal isn't indicative of a summon and we aren't currently selecting
+            if (rGrabbing && rh.PalmNormal.y < summonPalmNormThresh && !selecting)
+            {
+                selectionGestureDetected = true;
+                selecting = true;
+                Debug.Log("Selecting");
+            }
+
+            // if RH not in selecting motion and our onetimebool selecting is still true, set it to false
+            else if (!(rGrabbing && rh.PalmNormal.y < summonPalmNormThresh) && selecting)
+            {
+                selecting = false;
+                Debug.Log("Not Selecting anymore");
+            }
+        }
+        
+    }
+
+    Leap.Vector LeapToWorld (Leap.Vector lv)
+    {
+        lv.z *= 0.1f;
+        InteractionBox iBox = frame.InteractionBox;
+        Leap.Vector normalized = iBox.NormalizePoint(lv);
+        normalized += new Leap.Vector(0, 0, 0);
+        Debug.Log(normalized);
+        return normalized * leapToWorldMultiplier;
+    }
+
+
 
 
 }
